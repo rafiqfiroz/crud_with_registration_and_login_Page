1becfdc264d45c3525b7c26b173143db79bfea41
rafiqfiroz <rafiqahmed.com@gmail.com>
2024-07-31 11:41:48 +0600

    change login page

--- register.php
+++ register.php
@@ -1,23 +1,30 @@
 <?php
 session_start();
-date_default_timezone_set("Asia/Dhaka");
 include "config.php";
 
-if(isset($_POST['register'])){
+if (isset($_POST['register'])) {
     $name = $link->real_escape_string($_POST['name']);
     $username = $link->real_escape_string($_POST['username']);
     $password = $link->real_escape_string($_POST['password']);
     
     // Hash the password
-    $hashed_password = md5($password); // Change this to password_hash($password, PASSWORD_DEFAULT) for more security
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     
-    $sql = "INSERT INTO login (name, username, password) VALUES ('$name', '$username', '$hashed_password')";
+    // Check if the username already exists
+    $check_username_query = "SELECT * FROM login WHERE username='$username'";
+    $result = mysqli_query($link, $check_username_query);
     
-    if ($link->query($sql) === TRUE) {
-        $_SESSION['success'] = "Registration successful!";
-        header('location:login.php');
+    if (mysqli_num_rows($result) > 0) {
+        $_SESSION['error'] = "Username already exists. Please choose another one.";
     } else {
-        $_SESSION['error'] = "Error: " . $sql . "<br>" . $link->error;
+        $sql = "INSERT INTO login (name, username, password) VALUES ('$name', '$username', '$hashed_password')";
+    
+        if ($link->query($sql) === TRUE) {
+            $_SESSION['success'] = "Registration successful!";
+            header('location:login.php');
+        } else {
+            $_SESSION['error'] = "Error: " . $sql . "<br>" . $link->error;
+        }
     }
 }
 ?>
@@ -30,13 +37,14 @@ if(isset($_POST['register'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container">
   <div id="register">
     <div class="mb-4"><h1 class="text-center">Register</h1></div>
 
-    <?php if(isset($_SESSION['error'])){ ?>
+    <?php if (isset($_SESSION['error'])) { ?>
         <div class="alert alert-danger"><?php echo $_SESSION['error']; unset($_SESSION['error']); ?></div>
     <?php } ?>
 
