74eda09ad68b9c3f81fde12a056c7c87aae29813
rafiqfiroz <rafiqahmed.com@gmail.com>
2024-07-31 11:52:42 +0600

    make login page changes

--- create.php
+++ create.php
@@ -1,46 +1,53 @@
 <?php 
-    session_start();
-    include "authentication.php";
-    include "config.php";
+session_start();
+include "authentication.php";
+include "config.php";
 
-    if(isset($_POST['submit'])){
-        $name = $_POST['name'];
-        $sex = $_POST['sex'];
-        $phone = $_POST['phone'];
-        $email = $_POST['email'];
-        $photo = $_FILES['photo'];
-        $photo_name = $photo['name'];
-        $photo_temp = $photo['tmp_name'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $sex = $_POST['sex'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $password = md5($_POST['password']); // Encrypt the password
+    $photo = $_FILES['photo'];
+    $photo_name = $photo['name'];
+    $photo_temp = $photo['tmp_name'];
 
-        move_uploaded_file($photo_temp,'uploads/'.$photo_name);
+    // Move the uploaded photo to the 'uploads' directory
+    move_uploaded_file($photo_temp, 'uploads/' . $photo_name);
 
-        if(empty($photo_name)){
-            $sql = "INSERT INTO users (name,sex,phone,email,image) VALUES ('$name','$sex','$phone','$email','avatar.png')";
-        }else{
-            $sql = "INSERT INTO users (name,sex,phone,email,image) VALUES ('$name','$sex','$phone','$email','$photo_name')";
-        }
-        $query = mysqli_query($link,$sql);
-        if($query){
-            $log = getHostByName($_SERVER['HTTP_HOST']).' - '.date("F j, Y, g:i a").PHP_EOL.
-            "Record created_".time().PHP_EOL.
-            "---------------------------------------".PHP_EOL;
-            file_put_contents('logs/log_'.date("j-n-Y").'.log', $log, FILE_APPEND);
-
-            $_SESSION['success'] = "One record inserted successfully";
-            header('location:index.php');
-        }else{
-            $_SESSION['error'] = "Something is wrong, Record not inserted";
-            header('location:index.php');
-        }
+    // Insert data into the database
+    if (empty($photo_name)) {
+        $photo_name = 'avatar.png'; // Default photo if no photo is uploaded
     }
+
+    $sql = "INSERT INTO users (name, sex, phone, email, password, image) VALUES ('$name', '$sex', '$phone', '$email', '$password', '$photo_name')";
+    $query = mysqli_query($link, $sql);
+
+    if ($query) {
+        $log = getHostByName($_SERVER['HTTP_HOST']) . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
+            "Record created_" . time() . PHP_EOL .
+            "---------------------------------------" . PHP_EOL;
+        file_put_contents('logs/log_' . date("j-n-Y") . '.log', $log, FILE_APPEND);
+
+        $_SESSION['success'] = "User registered successfully";
+        header('location:index.php');
+        exit;
+    } else {
+        $_SESSION['error'] = "Something went wrong, Record not inserted";
+        header('location:index.php');
+        exit;
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add User</title>
+    <title>Register User</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -52,12 +59,12 @@
         <form action="" method="POST" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="name"><strong>Name</strong></label>
-                <input type="text" class="form-control" placeholder="Enter fullname" name="name" required>
+                <input type="text" class="form-control" placeholder="Enter full name" name="name" required>
             </div>
             <div class="form-group">
-                <label for="email"><strong>Sex</strong></label><br>
-                <input type="radio" name="sex" value="male"> Male &nbsp;
-                <input type="radio" name="sex" value="female"> Female
+                <label for="sex"><strong>Sex</strong></label><br>
+                <input type="radio" name="sex" value="male" required> Male &nbsp;
+                <input type="radio" name="sex" value="female" required> Female
             </div>
             <div class="form-group">
                 <label for="phone"><strong>Phone</strong></label>
@@ -66,6 +73,10 @@
             <div class="form-group">
                 <label for="email"><strong>Email</strong></label>
                 <input type="email" class="form-control" placeholder="Enter email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password"><strong>Password</strong></label>
+                <input type="password" class="form-control" placeholder="Enter password" name="password" required>
             </div>
             <div class="form-group">
                 <label for="photo"><strong>Photo</strong></label><br>
--- login.php
+++ login.php
@@ -1,55 +1,63 @@
 <?php 
-    session_start();
-    date_default_timezone_set("Asia/Dhaka");
-    include "config.php";
-    if(isset($_SESSION['id']) && !empty($_SESSION['id'])){
-        header('location:index.php');
-    }
-    
-    if(isset($_POST['login'])){
-        $username = $_POST['username'];
-        $password = md5($_POST['password']);
-        $captcha_field = $_POST['captcha'];
+session_start();
+include "config.php";
 
-        $sql = "SELECT * FROM login WHERE username='$username' AND password='$password'";
-        $query = mysqli_query($link,$sql);
+if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
+    header('location:index.php');
+    exit;
+}
 
-        if(mysqli_num_rows($query) == 1){
-            if($captcha_field == $_SESSION['captcha']){
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $captcha_field = $_POST['captcha'];
+
+    $sql = "SELECT * FROM login WHERE username='$username'";
+    $query = mysqli_query($link, $sql);
+
+    if (mysqli_num_rows($query) == 1) {
+        $data = mysqli_fetch_assoc($query);
+        if (password_verify($password, $data['password'])) {
+            if ($captcha_field == $_SESSION['captcha']) {
                 unset($_SESSION['captcha']);
 
-                $log = getHostByName($_SERVER['HTTP_HOST']).' - '.date("F j, Y, g:i a").PHP_EOL.
-                "Login_".time().PHP_EOL.
-                "---------------------------------------".PHP_EOL;
-                file_put_contents('logs/log_'.date("j-n-Y").'.log', $log, FILE_APPEND);
+                $log = getHostByName($_SERVER['HTTP_HOST']) . ' - ' . date("F j, Y, g:i a") . PHP_EOL .
+                    "Login_" . time() . PHP_EOL .
+                    "---------------------------------------" . PHP_EOL;
+                file_put_contents('logs/log_' . date("j-n-Y") . '.log', $log, FILE_APPEND);
 
-                if(isset($_POST['remember_me'])){
-                    setcookie('username', $username, time()+24*60*60);
-                    setcookie('password', $_POST['password'], time()+24*60*60);
-                }else{
-                    setcookie('username','');
-                    setcookie('password','');
+                if (isset($_POST['remember_me'])) {
+                    setcookie('username', $username, time() + 24 * 60 * 60);
+                    setcookie('password', $password, time() + 24 * 60 * 60);
+                } else {
+                    setcookie('username', '');
+                    setcookie('password', '');
                 }
-                $data = mysqli_fetch_assoc($query);
+
                 $_SESSION['id'] = $data['id'];
                 $_SESSION['name'] = $data['name'];
-                $_SESSION['timeout'] = time()+1800;
+                $_SESSION['timeout'] = time() + 1800;
                 $_SESSION['login_at'] = date('h:m:s a');
                 sleep(1);
                 header('location:index.php');
-            }else{
+                exit;
+            } else {
                 $_SESSION['error'] = 'Wrong captcha';
             }
-        }else{
+        } else {
             $_SESSION['error'] = 'Username or Password maybe wrong';
         }
+    } else {
+        $_SESSION['error'] = 'Username or Password maybe wrong';
     }
+}
 
-    $captcha_array = array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-    shuffle($captcha_array);
-    $captcha_code = substr(implode('',$captcha_array),0,6);
-    $_SESSION['captcha'] = $captcha_code;
+$captcha_array = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+shuffle($captcha_array);
+$captcha_code = substr(implode('', $captcha_array), 0, 6);
+$_SESSION['captcha'] = $captcha_code;
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -63,8 +71,7 @@
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <title>Login</title>
     <style>
-        #login
-        {
+        #login {
             margin: 18% 30%;
             border: 1px solid lightgray;
             padding: 10px 20px 40px;
@@ -77,15 +84,16 @@
   <div id="login">
     <div class="mb-4"><h1 class="text-center">Login</h1></div>
 
-    <?php if(isset($_SESSION['error'])){ ?>
+    <?php if (isset($_SESSION['error'])) { ?>
         <div class="alert alert-danger"><?php echo $_SESSION['error']; unset($_SESSION['error']); ?></div>
     <?php } ?>
+
     <form action="" method="POST">
         <div class="input-group mb-3">
             <div class="input-group-prepend">
                 <span class="input-group-text"><i class='fas fa-user'></i></span>
             </div>
-            <input type="text" class="form-control" placeholder="Enter Username" id="username" name="username" value="<?php echo isset($_COOKIE['username']) ? $_COOKIE['username'] : '' ?>" requied>
+            <input type="text" class="form-control" placeholder="Enter Username" id="username" name="username" value="<?php echo isset($_COOKIE['username']) ? $_COOKIE['username'] : '' ?>" required>
         </div>
 
         <div class="input-group mb-3">
@@ -110,28 +118,35 @@
         </div>
 
         <button type="submit" class="btn btn-primary btn-block" name="login">Login</button>
+        
+        <div class="text-center mt-3">
+            <a href="register.php" class="btn btn-secondary">Register</a>
+        </div>
     </form>
   </div>
 </div>
 </body>
 </html>
 <script type="text/javascript">
-    //Password visibility
-    function passwordToggle(){
+    // Password visibility
+    function passwordToggle() {
         var btn = document.getElementById('toggle-btn');
         var pw = document.getElementById('password');
 
-        if(pw.type == 'password'){
-            pw.type = 'text'
-            btn.innerHTML = "<i class='far fa-eye'></i>"
-        }else{
-            pw.type = 'password'
-            btn.innerHTML = "<i class='far fa-eye-slash'></i>"
+        if (pw.type == 'password') {
+            pw.type = 'text';
+            btn.innerHTML = "<i class='far fa-eye'></i>";
+        } else {
+            pw.type = 'password';
+            btn.innerHTML = "<i class='far fa-eye-slash'></i>";
         }
     }
 
-    //Error message hide
-    setTimeout(function(){
-        document.getElementsByClassName('alert')[0].style.display = 'none';
+    // Error message hide
+    setTimeout(function () {
+        var alert = document.getElementsByClassName('alert')[0];
+        if (alert) {
+            alert.style.display = 'none';
+        }
     }, 3000);
 </script>
